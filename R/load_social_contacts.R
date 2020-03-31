7ad054dfb732b59d094b123d257cd82f6a920791
Jim Duggan <jim@MyMacBook.local>
2020-03-31 23:26:59 +0100

    Update initial test

--- R/load_social_contacts.R
+++ R/load_social_contacts.R
@@ -3,14 +3,16 @@ library(socialmixr)
 library(stringr)
 
 load_social_contacts <- function(){
-  lower_lims <- str_split(age_data$AgeCohort, "-|\\+") %>%
+  lower_lims <- stringr::str_split(age_data$AgeCohort, "-|\\+") %>%
   sapply(function(interval) interval[[1]])
 
   age_data$lower.age.limit <- as.numeric(lower_lims)
 
-  suppressMessages(scenario_1 <- contact_matrix(polymod, age.limits = c(0, 5, 15, 65),
-                              countries = "Great Britain",
-                              survey.pop = age_data, symmetric = TRUE))
+  suppressMessages(scenario_1 <- socialmixr::contact_matrix(socialmixr::polymod,
+                                                            age.limits = c(0, 5, 15, 65),
+                                                            countries = "Great Britain",
+                                                            survey.pop = age_data,
+                                                            symmetric = TRUE))
 
   data_env$IRL_POLYMOD <- scenario_1
 }
